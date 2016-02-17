2e444d65c5097ccd915b1cdbe7d59a77759507b4
root <root@ik1-302-11037.vs.sakura.ne.jp>
2016-02-18 03:24:54 +0900

    add pkg

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # playbook-ik
 さくらのVPS（石狩）のCentOS6.7を構築するときに使うplaybook
+
+# ansible-playbook
+
+## pkg.yml
+
+- yumでパッケージを追加
+
+```
+ansible-playbook pkg.yml
+```
---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+yum install -y python-devel
+cd /tmp ; wget https://bootstrap.pypa.io/get-pip.py
+cd /tmp ; python get-pip.py ; rm get-pip.py
+pip install ansible
